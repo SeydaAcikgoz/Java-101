c2308d9860cdd79f61e3c6910de3fab69c5f7d95
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-02-28 14:17:39 +0300

    Create kdv_oran_hesaplama.java
    https://www.patika.dev

---
+++ kdv_oran_hesaplama.java
@@ -0,0 +1,26 @@
+package patika;
+
+import java.util.Scanner;
+
+public class kdv_tutari_hesaplama {
+
+	public static void main(String[] args) {
+		
+		double tutar,kdvOran=0.18,kdvTutar,kdvliTutar;
+		Scanner veri=new Scanner(System.in);
+		
+		System.out.print("Tutari giriniz:");
+		tutar=veri.nextDouble();
+		
+		kdvTutar=tutar*kdvOran;
+		kdvliTutar=tutar+kdvTutar;
+		
+		System.out.println("Kdv'siz tutar:"+tutar);
+		System.out.println("Kdv orani tutar:"+kdvOran);
+		System.out.println("Kdv tutari:"+kdvTutar);
+		System.out.println("Kdv'li tutar:"+kdvliTutar);
+
+		
+	}
+
+}
