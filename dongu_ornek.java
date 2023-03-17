93a79f278673726bf9fdf7d92b16b2173f94b0ca
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-17 19:04:50 +0300

    Update dongu_ornek.java

--- dongu_ornek.java
+++ dongu_ornek.java
@@ -1,18 +1,21 @@
-package patika;
-
 import java.util.Scanner;
 
 public class dongu_ornek {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
+		int toplam=0,adet=0;
+		
 		System.out.print("Bir sayi giriniz:");
 		int sayi = input.nextInt();
 
 		for (int i = 0; i < sayi; i++) {
 			if (i % 3 == 0 && i % 4 == 0) {
+				toplam+=i;
+				adet++;
 				System.out.print(i + "\t");
 			}
 		}
+		
+		System.out.println("Ortalama:"+(toplam/adet));
 	}
 }
