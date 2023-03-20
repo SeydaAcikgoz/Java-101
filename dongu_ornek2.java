4eb4f88590313e58a786945b7b4698740f1a7239
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-20 14:53:52 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ dongu_ornek2.java
@@ -0,0 +1,29 @@
+package patika;
+
+import java.util.Scanner;
+
+public class dongu_ornek2 {
+
+	public static void main(String[] args) {
+		// Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul
+		// eden
+		// ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana
+		// basan programı:
+
+		Scanner input = new Scanner(System.in);
+		int toplam = 0, sayi;
+
+		do {
+			System.out.print("\nBir sayi giriniz:");
+			sayi = input.nextInt();
+			if (sayi % 2 == 0 && sayi % 4 == 0) {
+				System.out.print(sayi + "\t");
+				toplam += sayi;
+			}
+		} while (sayi % 2 == 0);
+
+		System.out.print("Toplam:" + toplam);
+
+	}
+
+}
