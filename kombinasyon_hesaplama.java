57b0ec0ec0d2adbf99914153a5aaf503dcb4e67b
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-21 22:00:22 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ kombinasyon_hesaplama.java
@@ -0,0 +1,28 @@
+package patika;
+
+import java.util.Scanner;
+
+public class kombinasyon_hesaplama {
+
+	public static void main(String[] args) {
+		// C(n,r) = n! / (r! * (n-r)!)
+		Scanner input=new Scanner(System.in);
+		
+		System.out.print("C(n,r):");
+		System.out.println("n'i giriniz:");
+		int n=input.nextInt();
+		System.out.println("r'i giriniz:");
+		int r=input.nextInt();
+		
+		System.out.println("C("+n+","+r+"):"+(faktoriyel(n))/(faktoriyel(r)*(faktoriyel(n-r))));
+	}
+
+	public static int faktoriyel(int sayi) {
+		int faktoriyel = 1;
+		for (int i = 1; i <= sayi; i++) {
+			faktoriyel*=i;
+		}
+		return faktoriyel;
+	}
+
+}
