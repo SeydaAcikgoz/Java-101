e0dc46ba4e2768f58ea31eb2602f46b4e1499305
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-26 23:34:37 +0300

    Update harmonik_sayi.java

--- harmonik_sayi.java
+++ harmonik_sayi.java
@@ -1,5 +1,18 @@
-package patika;
+import java.util.Scanner;
 
 public class harmonik_sayi {
+  public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double result=0.0;
+		
+        System.out.print("Sayi Giriniz :");
+        int number = input.nextInt();
+        
+        for(int i=1;i<=number;i++) {
+        	result+=(1.0/i);//sonucun double cıkmasi icin double olan 1.0 a boleriz
+        }
 
+        System.out.println(result);
+	}
 }
