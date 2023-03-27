992a9a321356664cbaca9146d7a9b9640441188f
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-27 18:35:47 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ ebob_ekok.java
@@ -0,0 +1,49 @@
+package patika;
+
+import java.util.Scanner;
+
+public class ebob_ekok {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int ebob=1,ekok=1;
+
+		System.out.print("1.Sayiyi Giriniz :");
+        int number1 = input.nextInt();
+        
+        System.out.print("2.Sayiyi Giriniz :");
+        int number2 = input.nextInt();
+        
+        if(number1<number2) {
+        	
+        	int i=1;
+        	while(i<=number1) {
+        		if(number1%i==0 && number2%i==0) {
+        			ebob=i;
+        		}
+        		i++;
+        	}
+        }else {
+        	int i=1;
+        	while(i<=number2) {
+        		if(number1%i==0 && number2%i==0) {
+        			ebob=i;
+        		}
+        		i++;
+        	}
+        }
+        System.out.println(number1+" ve "+number2+" nin ebobu: "+ebob);
+        
+        int k=1;
+        while(k<=number1*number2) {
+        	if(k%number1==0 && k%number2==0) {
+    			ekok=k;
+    			break;
+    		}
+        	k++;
+        }
+        System.out.println(number1+" ve "+number2+" nin ekoku: "+ekok);
+
+	}
+
+}
