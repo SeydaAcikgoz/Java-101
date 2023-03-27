b80df3c43e93a99a5051f3e544d42ed508b28a3b
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-28 02:35:15 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ min_max_bulma.java
@@ -0,0 +1,32 @@
+package patika;
+
+import java.util.Scanner;
+
+public class min_max_bulma {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Kac tane sayi gireceksiniz: :");
+        int piece = input.nextInt();
+        
+        int max=-1000,min=1000;
+        for(int i=0;i<piece;i++) {
+        	System.out.print((i+1)+".sayiyi Giriniz :");
+            int number = input.nextInt();
+            
+            if(number>max) {
+            	max=number;
+            }
+            
+            if(number<min) {
+            	min=number;
+            }
+        }
+        
+        System.out.println("En buyuk sayi:"+max);
+        System.out.println("En kucuk sayi:"+min);
+
+	}
+
+}
