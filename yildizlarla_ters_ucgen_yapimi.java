bffa4156defe109eb861067fd673b322803df1e3
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-29 14:19:13 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ yildizlarla_ters_ucgen_yapimi.java
@@ -0,0 +1,27 @@
+package patika;
+
+import java.util.Scanner;
+
+public class yildizlarla_ters_ucgen_yapimi {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Yukseklik kac satir olsun?");
+		int yukseklik = input.nextInt();
+		
+		for (int i = yukseklik ; i > 0; i--) {
+			for (int j = 1; j <= yukseklik - i; j++) {
+				System.out.print(" ");
+			}
+				
+
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
