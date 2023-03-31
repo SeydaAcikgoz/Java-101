f2a46e19a426e6f3917f9f2eadeead1250594c7d
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-31 13:57:58 +0300

    Add files via upload
    [Patika.dev](https://www.patika.dev)

---
+++ fibonacci_serisi.java
@@ -0,0 +1,19 @@
+package patika;
+
+public class fibonacci_serisi {
+	public static void main(String args[]) {
+		int sayi = 11;
+
+		int n1 = 0, n2 = 1, n3;
+		//System.out.println(n1 + "+" + n2+"="+(n1+n2));
+
+		for (int i = 2; i < sayi; ++i) {
+			n3 = n1 + n2;
+			System.out.println(n1+"+"+n2+"=" + n3);
+			n1 = n2;
+			n2 = n3;
+		}
+	}
+
+	
+}
